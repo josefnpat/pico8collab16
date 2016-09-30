1eaf48c89f06bd75f79a25c275ad462ef1fe9180
Manuel Calavera <various.artists@yahoo.com>
2016-09-30 03:05:25 +0100

    always display highscore

--- collab_2/lua/07.lua
+++ collab_2/lua/07.lua
@@ -5,8 +5,10 @@ draw_rotated_sprite = function(spr, spr_x, spr_y, spr_ang)
   for y=-6,5 do 
     for x=-6,5 do
       ox,oy=( s*y+c*x)/b, (-s*x+c*y)/b
-      colr=sget(spr%16*8+ox+4,flr(spr/16)*8+oy+4)
-      ax,ay=ox+4,oy+4
+
+      ax,ay,
+      colr=ox+4,oy+4,
+        sget(spr%16*8+ox+4,flr(spr/16)*8+oy+4)
 
       if ax>=0 and ax<8 and ay>=0 and ay<8 and colr>0 then 
         pset(spr_x+4+x,spr_y+4+y,colr)
@@ -40,10 +42,10 @@ _draw = function(self)
   map(0, 16, 0, 24, 16, 32)
 
   if death_y>1 then
-    print(score, 18, 104)
+    print(score, 10, 104)
     spr(43,death_x,death_y)
   else
-  	print("alternate ‹ and ‘ to speed up.\n in the air press ‹ and ‘ to\n spin and — ƒ ” for tricks!\n     press Ž to try again", 1, 100)
+    print("alternate ‹ and ‘ to speed up.\n in the air press ‹ and ‘ to\n spin and — ƒ ” for tricks!\n     press Ž to try again", 1, 100)
   end
   draw_rotated_sprite(player_sprite,player_x,player_y,player_angle)
 end,
@@ -94,8 +96,8 @@ _update = function(self)
     player_angle = 0.125
   else
     death_x = 1
-    force *= 1.1 
-    player_y-=player_speed*1.2 
+    force *= 1.09
+    player_y-=player_speed
     player_y+=force
     player_x+=player_speed
     --sfx"0" -- using someone else's sfx, woo. edit: ehh too much
@@ -110,7 +112,7 @@ _update = function(self)
       player_sprite,
       death_y,
       player_angle,
-      force = 999,8,2,0,""
+      force = 999,8,2,0,"\nhighscore remains: " .. dget(29)
       if score > dget(29) then
         dset(29, score)
         force = "\n     new highscore!"
@@ -118,13 +120,14 @@ _update = function(self)
       else
         sfx"12" -- using someone else's sfx, woo
       end     
-      score = "nice jump! score: " .. score .. force
+      score = " nice jump! score: " .. score .. force
     else
       death_x,
       death_y,
-      player_sprite,player_angle = player_x + 2,player_y,10,0
+      player_sprite,
+      player_angle,
+      score = player_x + 2,player_y,10,0,"    ouch ouch ouch..."
       sfx"16" -- using someone else's sfx, woo
-      score = "    ouch ouch ouch..."
     end
   end  
 end
