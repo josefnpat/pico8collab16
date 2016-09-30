33b3d3ce61f6f484c5309500619faca0a68dabe7
teamdisposable <michael@crunchpress.co.uk>
2016-09-30 22:27:53 +0100

    updated code for videopirate v3

--- collab_2/lua/15.lua
+++ collab_2/lua/15.lua
@@ -1,138 +1,148 @@
-add(games,{
-		name="videopirate",
-		author="team_disposable",
-		_update = function()
+add(games,{name="videopirate",author="team_disposable",
+
+_update = function()
 			cls()
 
-	if(gameover == false) then
+	if notgameover then
 
-		if(cliptimer <= 0) then
-			
-			if(clipqueue[1] != null) then
+		if cliptimer <= 0 then
+					
+			if (#obs > 0 or success == 15) notgameover = false
 				
-			sprite1,sprite2,val1,val2,val3,val4,nextclip = {5,5,32,40,rnd(4),(flr(rnd(6.9))+1) * 8},{6,6,60,40,rnd(4),(flr(rnd(6.9))+1) * 8},0,0,0,0,clipqueue[#clipqueue]
 			
-			del(clipqueue,nextclip)
-			cliptimer += nextclip[2]
+			size1,size2,rand = rnd"6.9"*8,rnd"6.9"*8,rnd"3.9"
 			
-				if(nextclip[1] == 0) val1,val2,val3,val4 = 32,64,40,72
+			obs = {}
+				for i=success,-1,-1 do
+
+					add(obs,{rnd"4.5",flr(rnd"4"),11})
+
+				end
 				
-				if(nextclip[1] == 1) val1,val2,val3,val4 = 32,64,40,64
-
-				if(nextclip[1] == 2) val1,val2,val3,val4 = 32,72,40,80
-
-				if(nextclip[1] == 3) val1,val2,val3,val4 = 32,80,40,64
-
-				sprite1[1],sprite1[2], sprite2[1],sprite2[2] = val1,val2,val3,val4
-			else
-
-				gameover = true
-				--end the game
-
-			end
-
+		
+				
+			cliptimer += 100
+			
+				sprite1x,sprite1y,sprite2x,sprite2y = 32,64,40,64
+				
+				if(rand < 3) sprite1y = 80 
+				
+				if(rand < 2) sprite1y,sprite2y = 72,80
+				
+				if(rand < 1) sprite1y,sprite2y = 64,72
 
 		end -- end of cliptimer is 0
 
 		--draw the clip
 
-	adjust = rnd(8)
-	adjust2 = rnd(8)
+	adjust,adjust2 = rnd"8",rnd"8"
 	
-	sspr(sprite1[1],sprite1[2],8,8,sprite1[3]+adjust,sprite1[4]+adjust2,sprite1[6],sprite1[6])
+	
+	sspr(sprite1x,sprite1y,8,8,32+adjust,40+adjust2,size1,size1)
 		--sx sy sw sh dx dy [dw dh] [flip_x] [flip_y]
-	sspr(sprite2[1],sprite2[2],8,8,sprite2[3]+adjust2,sprite2[4]+adjust,sprite2[6],sprite2[6])
+	sspr(sprite2x,sprite2y,8,8,60+adjust2,40+adjust,size2,size2)
 		
 
 		--update obstructions
-	obs = nextclip[3]
+	--obs = nextclip[3]
 	if(#obs == 0) success += 1  cliptimer = 0 
 	for o in all(obs) do
-	
-		--{typeofobstruction,directionofobstruction,burstfreq,burstduration,burst_delaystart})
-		if(o[1] < 1) then 
 		
-			--static
+		if o[1] < 0.5 then 
 			
-			--controls to fix
-			if(btn(o[2])) o[3] -= 0.3  
+				--copyright symbol
+				copyrightnotshowing = false
+				if (btnp"5" and btnp"4") o[3] = 0  copyrightnotshowing=true
+				sspr(32,88,8,8,40,50,40,40)
+				--spr(180,50,50)
 			
-			 
+		end
+		
+		if copyrightnotshowing then 
+			if o[1] < 1.5 then
+				
+					if(btnp"4") o[3] -= 1
+					liney = rnd"40"+29
+					for i = 19,109,5 do
+
+						line(i,liney,i+o[3],liney+o[3],14)
+
+					end
 			
-			for i=1,o[3],1 do
-				for j=1,40,1 do
+			
+			elseif o[1] < 3.5 then
+				--static
+				
+					--controls to fix
+					if(btn"o[2]") o[3] -= 0.5  
 					
-					pset(rnd(90)+19,rnd(80)+19,3)
-					
-					
-				end
+					 
+					for i=1,o[3],1 do
+						for j=1,20,1 do
 							
+							pset(rnd"90"+19,rnd"80"+19,o[2]+1)
+							
+							
+						end
+									
+					end
+				
+					
+			elseif o[1] < 4.5 then
+				
+				bumpat -= 2
+				if(bumpat < 19) bumpat = 109
+				
+				bmpcolour = 9
+				if bumpat < 74 and bumpat > 54 then
+					bmpcolour = 11
+					if btnp"5" then o[3] = 0 end
+				
+				end
+				
+				for b = 40+adjust,80,5 do
+					line(19,b,bumpat,b,bmpcolour)
+					line(bumpat+8,b,109,b)
+					spr(181,bumpat,b-8)
+				end
+				
 			end
-			
-			
-		
-		else
-	
-		--lines
-		if(btnp(4)) o[3] -= 1
-		liney = rnd(40)+29
-		for i = 19,109,5 do
-
-			line(i,liney,i+o[3],liney+o[3],14)
-
 		end
-		
-		
-		
-		end
-		if(o[3] <= 0) del(obs,o)
+		if(o[3] <= 0.1) del(obs,o) cliptimer += 20
 	
 	end
-		
-
+	
 	--draw the tv
 
 	sspr(48,80,16,16,4,4,120,120)	
 		
-		
-
 		cliptimer -=1
-
-		print(cliptimer,100,10,15)
+		print(cliptimer,85,20,15)
+			
 
 	else
+		
+		xval,yval,printline = 80,100,"better luck next time."
+		for i=1,success,1 do
+		printline = "your collection is superb. \nvhs will never die, you gloat"
+			sspr(48,64,16,8,xval,yval,48,24)
+			yval -=4
+			xval -=4
 	
-		print("you pirated "..success.." out of "..total.." tapes",5,50,15)
+		end
+		print(printline,5,20,15)
+		
 	
 	end
 
-
-		end,
+	spr(134,30,110,2,1)
+	print(success,50,110)
+	
+	
+end,
 		
-	_init = function(self)
+_init = function(self)
 			
-		clipqueue,cliptimer,gameover,success = {},0,false,0
-
-		nextclip = null
-		--difficulty = 10
-		sprite1= null
-		sprite2 = null
-
-
-			--populate queue
-			for difficulty = 10,0,-1 do
-				
-				obstructions = {}
-				for i=1,difficulty,1 do
-
-					add(obstructions,{rnd(1.9),flr(rnd(3.9)),difficulty})
-
-				end
-				add(clipqueue,{flr(rnd(3.9)),difficulty * 50,obstructions})
-
-			end
-
-			total = #clipqueue
-
-		end
-}) -- game 15
+		notgameover,cliptimer,success,bumpat,copyrightnotshowing,obs = true,0,0,0,true,{}
+		music"0"
+	end})
