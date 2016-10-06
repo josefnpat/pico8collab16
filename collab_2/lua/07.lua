ffb18bec1163f05015cd6359a9f3f700a6f66995
Manuel Calavera <various.artists@yahoo.com>
2016-10-06 20:22:05 +0100

    Adjust scoring

--- collab_2/lua/07.lua
+++ collab_2/lua/07.lua
@@ -103,14 +103,14 @@ _update = function(self)
     player_y-=player_speed
     player_y+=force
     player_x+=player_speed
-    --sfx"0" -- using someone else's sfx, woo. edit: ehh too much
+    --sfx"0" -- using someone else's sfx, woo. edit: ehh too much sound
   end 
 
   if player_y>71 then
     player_y,
     flips = 71,-1
     if (player_angle+0.1)%1 < 0.23 then
-      score += player_speed*25 + abs(player_angle)*50
+      score += player_speed*25 + abs(player_angle-0.125)*50
       death_x,
       player_sprite,
       death_y,
@@ -119,9 +119,9 @@ _update = function(self)
       if score>dget"29" then
         dset(29, score)
         force = "\n     new highscore!"
-        sfx"17" -- using someone else's sfx, woo
+        sfx"17" -- using someone else's sfx, woo 
      -- else
-       -- sfx"12" -- using someone else's sfx, woo
+       -- sfx"12" -- using someone else's sfx, woo. edit: too many tokens!
       end     
       score = " nice jump! score: " .. score .. force
     else
@@ -129,7 +129,7 @@ _update = function(self)
       death_y,
       player_sprite,
       player_angle,
-      score = player_x + 2,player_y,10,0,"    ouch ouch ouch..."
+      score = player_x,player_y,10,0,"    ouch ouch ouch..."
       sfx"2" -- using someone else's sfx, woo
     end
   end  
