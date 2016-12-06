a55007fc76ce83bdb76675de193080185ca4b7c4
Lemtzas <lemtzas@gmail.com>
2016-12-05 18:19:32 -0800

    adding restart message. fixing bugs.

--- collab_2/lua/14.lua
+++ collab_2/lua/14.lua
@@ -4,10 +4,7 @@ add(games,{
  ----------------------- init --
  _init = function(self)
   -- polluting the globals with my data
-  t,food = 1,0
-  -- snek
-  s_dir, s_x,s_y = nil, 10,10
-  s_len, s_tail, s_run_id = 3, {}, flr(rnd"32767")
+  t,food,s_dir,s_x,s_y,s_len,s_tail,s_run_id,s_done,input= 1,0,nil,10,10,3, {}, flr(rnd"32767"),false,{}
 
   -- map initialization
   for x=0,63 do
@@ -17,90 +14,146 @@ add(games,{
   end
  end,
 
- update_snek = function(self)
+ update_snek = function(self,input)
   if s_done then
     local tail_end = s_tail[1]
     del(s_tail,tail_end)
     return tail_end and sset(tail_end.x,tail_end.y,8)
   end
 
-  -- input
-  s_dir =
-   btn"0" and s_dir ~= 1 and 0 or
-   btn"1" and s_dir ~= 0 and 1 or
-   btn"2" and s_dir ~= 3 and 2 or
-   btn"3" and s_dir ~= 2 and 3 or
-   s_dir
-  if btn"4" then self:_init() end
-  if not s_dir then return end
+  if input then
+    s_dir = input
 
-  local p_x,p_y = s_x,s_y
-  s_x =
-   s_dir == 0 and s_x - 1 or
-   s_dir == 1 and s_x + 1 or
-   s_x
+    p_x,p_y,s_x,s_y =
+      s_x,s_y,
+      s_dir == 0 and s_x - 1 or
+      s_dir == 1 and s_x + 1 or
+      s_x,
+      s_dir == 2 and s_y - 1 or
+      s_dir == 3 and s_y + 1 or
+      s_y
 
-  s_y =
-   s_dir == 2 and s_y - 1 or
-   s_dir == 3 and s_y + 1 or
-   s_y
+    -- pausing on walls
+    if s_x < 0 or s_x > 63 or
+       s_y < 0 or s_y > 63 then
+     s_x,s_y = p_x,p_y
+     return
+    end
 
-  -- pausing on walls
-  if s_x < 0 or s_x > 63 or
-     s_y < 0 or s_y > 63 then
-   s_x,s_y = p_x,p_y
-   return
-  end
+    -- sliding
+    s_tail[#s_tail+1] =
+      {
+        x = p_x,
+        y = p_y
+      }
+    sset(s_tail[1].x,s_tail[1].y,0)
+    sset(s_tail[#s_tail].x,s_tail[#s_tail].y,6)
+    if #s_tail > s_len then
+      for i=1,#s_tail do
+        s_tail[i] = s_tail[i+1]
+      end
+    end
 
-  -- sliding
-  s_tail[#s_tail+1] =
-   {
-    x = p_x,
-    y = p_y
-   }
-  local old = s_tail[1]
-  local new = s_tail[#s_tail]
-  sset(old.x,old.y,0)
-  sset(new.x,new.y,6)
-  if #s_tail > s_len then
-   for i=1,#s_tail do
-    s_tail[i] = s_tail[i+1]
+    local target = sget(s_x,s_y)
+    if target == 11 then
+      s_len = s_len + 1
+      food = food - 1
+{
+ name = "snek over 16",
+ author = "lemtzas\nhiscore: " .. dget"42",
+ ----------------------- init --
+ _init = function(self)
+  -- polluting the globals with my data
+  t,food,s_dir,s_x,s_y,s_len,s_tail,s_run_id,s_done,input= 1,0,nil,10,10,3, {}, flr(rnd"32767"),false,{}
+
+  -- map initialization
+  for x=0,63 do
+   for y=0,63 do
+    sset(x,y,0)
    end
   end
+ end,
 
-  local target = sget(s_x,s_y)
-  if target == 11 then
-    s_len = s_len + 1
-    food = food - 1
-    if s_len > dget"42" then
-      s_winning = dget"43" ~= s_run_id and sfx"47" or true
-      dset(42,s_len)
-      dset(43,s_run_id)
-    end
-    sfx(s_len % 10 == 0 and 45 or 44)
-  elseif
-    s_dir ~= nil and
-    target ~= 0 then
-    sfx"46"
-    s_done = true
+ update_snek = function(self,input)
+  if s_done then
+    local tail_end = s_tail[1]
+    del(s_tail,tail_end)
+    return tail_end and sset(tail_end.x,tail_end.y,8)
   end
 
-  sset(s_x,s_y,8)
+  if input then
+    s_dir = input
+
+    p_x,p_y,s_x,s_y =
+      s_x,s_y,
+      s_dir == 0 and s_x - 1 or
+      s_dir == 1 and s_x + 1 or
+      s_x,
+      s_dir == 2 and s_y - 1 or
+      s_dir == 3 and s_y + 1 or
+      s_y
+
+    -- pausing on walls
+    if s_x < 0 or s_x > 63 or
+       s_y < 0 or s_y > 63 then
+     s_x,s_y = p_x,p_y
+     return
+    end
+
+    -- sliding
+    s_tail[#s_tail+1] =
+      {
+        x = p_x,
+        y = p_y
+      }
+    sset(s_tail[1].x,s_tail[1].y,0)
+    sset(s_tail[#s_tail].x,s_tail[#s_tail].y,6)
+    if #s_tail > s_len then
+      for i=1,#s_tail do
+        s_tail[i] = s_tail[i+1]
+      end
+    end
+
+    local target = sget(s_x,s_y)
+    if target == 11 then
+      s_len = s_len + 1
+      food = food - 1
+      if s_len > dget"42" then
+        if dget"43" ~= s_run_id then sfx"47" end
+        dset(42,s_len)
+        dset(43,s_run_id)
+      end
+      sfx(s_len % 10 == 0 and 45 or 44)
+    else
+      s_done = s_dir ~= nil and target ~= 0 and not sfx"46"
+    end
+
+    sset(s_x,s_y,8)
+  end
  end,
 
  --------------------- update --
  _update = function(self)
-  function r_grid()
-   return flr(rnd"63")
-  end
   t=t+1
+  -- input
+  s_pdir = s_ndir
+  s_ndir =
+    btn"0" and s_pdir ~= 1 and 0 or
+    btn"1" and s_pdir ~= 0 and 1 or
+    btn"2" and s_pdir ~= 3 and 2 or
+    btn"3" and s_pdir ~= 2 and 3 or
+    s_ndir
+  if s_pdir ~= s_ndir then add(input, s_ndir) end
 
   if t % 2 == 0 then
-   self:update_snek()
+    self:update_snek(input[1] or s_dir)
+    self:update_snek(input[2])
+
+    input = {}
   end
 
   if t % 30 == 0 and food < 10 then
-   local x,y = r_grid(), r_grid()
+   local x,y = flr(rnd"63"),flr(rnd"63")
    sset(x,y,sget(x,y) == 0 and 11 or 0)
    food = food + 1
   end
@@ -109,8 +162,14 @@ add(games,{
 ------------------------ draw --
  _draw = function(self)
   cls()
-  print("hiscore " .. dget"42", 1, 1, s_winning and 12 or 1)
+  print("hiscore " .. dget"42", 1, 1, dget"43" == s_run_id and 12 or 1)
   print("  score " .. s_len, 1, 7, 1)
+
+  if s_done then
+    if btn"4" or btn"5" then self:_init() end
+    print("— to restart", 37, 122, 8)
+  end
+
   sspr(0,0,64,64,0,0,128,128)
  end
 })
